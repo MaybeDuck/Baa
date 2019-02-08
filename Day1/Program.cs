fe26f5b4785e197a9c15e60d2a4c6dde5d60428b
Blake Freeman <blakefreeman@Blakes-MacBook-Air.local>
2019-02-07 18:42:43 -0600

    Created SHeep counter

---
+++ Day1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Day1
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+
+            Console.WriteLine("Type a number");
+
+           string userInput = Console.ReadLine();
+            int.TryParse(userInput, out int userNumber);
+           
+            Random random = new Random();
+            //Console.WriteLine(random.Next(1, 2));
+            for (int i = 0; i < userNumber; i++)
+            {
+                if (random.Next(1, 2) == 1)
+                {
+                    Console.WriteLine("Baa");
+                }
+                if (random.Next(1, 3) == 2)
+                {
+
+                    Console.Write(i + " Sheep\n");
+                }
+
+
+            }
+        }
+
+    }
+}
